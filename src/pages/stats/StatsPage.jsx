6a9a94312e6b906289e0efbc4254b861f7ecec5e
Tanvirr0777 <montasirtanvir0777@gmail.com>
2026-04-14 23:59:34 +0600

    chart added

--- src/pages/stats/StatsPage.jsx
+++ src/pages/stats/StatsPage.jsx
@@ -1,6 +1,7 @@
 import React, { useContext } from 'react';
-import { Legend, Pie, PieChart } from 'recharts';
+import { Legend, Pie, PieChart, Tooltip } from 'recharts';
 import { FriendContext } from '../../context/ContextCreate';
+import NodataFound from '../timelinePage/NodataFound';
 
 const StatsPage = () => {
 
@@ -8,38 +9,56 @@ const StatsPage = () => {
 
     const calls = toTimeline.filter(call => call.medium === "call")
     console.log(calls);
+    const video = toTimeline.filter(call => call.medium === "video")
+    console.log(calls);
+    const text = toTimeline.filter(call => call.medium === "text")
+    console.log(calls);
+
+    const callvalue = calls.length;
+    const textvalue = text.length;
+    const videovalue = video.length;
 
     const data = [
-  { name: 'Group A', value: 400, fill: '#0088FE' },
-  { name: 'Group B', value: 300, fill: '#00C49F' },
-  { name: 'Group C', value: 300, fill: '#FFBB28' },
+  { name: 'Call', value: callvalue, fill: '#0088FE' },
+  { name: 'Text', value: textvalue, fill: '#00C49F' },
+  { name: 'Video', value: videovalue, fill: '#FFBB28' },
 ];
 
     return (
         <div className='my-20 w-[75%] mx-auto'>
             <h2 className='text-4xl font-bold'>Friendship Analytics</h2>
-            <div className='bg-white p-3 my-3 rounded-xl'>
+            <div className='bg-white p-3 px-5 my-3 rounded-xl'>
                 <h2 className='text-xl'>By Interaction Type</h2>
 
+          {
+            (callvalue+textvalue+videovalue) === 0 ? (
+            <div className='my-5'>
+                 <NodataFound />
+            </div>):
+            (
+                <div className='w-[35%] h-[30%] mx-auto py-8'>
 
-              <div className='w-[35%] h-[30%] mx-auto py-8'>
-
- <PieChart style={{ width: '100%', maxWidth: '500px', maxHeight: '80vh', aspectRatio: 1 }} responsive>
-      <Pie
-        data={data}
-        innerRadius="80%"
-        outerRadius="100%"
-        cornerRadius="50%"
-        fill="#8884d8"
-        paddingAngle={5}
-        dataKey="value"
-        isAnimationActive={true}
-      />
-       <Legend className='my-4' />
+          <PieChart style={{ width: '100%', maxWidth: '500px', maxHeight: '80vh', aspectRatio: 1 }} responsive>
+         <Pie
+          data={data}
+          innerRadius="80%"
+          outerRadius="100%"
+          cornerRadius="50%"
+          fill="#8884d8"
+          paddingAngle={5}
+          dataKey="value"
+          isAnimationActive={true}
+        />
+        <Tooltip />
+       <Legend className='mt-4' />
     </PieChart>
             
             </div>   
    
+            ) 
+          }
+
+
             </div>
         </div>
     );
